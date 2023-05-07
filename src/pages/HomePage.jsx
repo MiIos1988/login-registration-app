89c991c30b085b4995e539918442307bf98f2b5a
Milos1988 <vojvoda1988>
2023-05-07 18:00:02 +0200

    Delate btn

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import { getAllData } from "../service/service";
+import moment from "moment";
 
 const HomePage = () => {
   const [ipAddress, setIpAddress] = useState();
+  const [information, setInformation] = useState({
+    country: "",
+    city: "",
+  });
 
   useEffect(() => {
     getAllData()
@@ -10,37 +15,77 @@ const HomePage = () => {
       .catch((error) => console.log(error));
   }, []);
 
-  const findData = () => {
-    console.log("work");
+  const findData = (ip) => {
+    fetch(`https://ipapi.co/${ip}/json/`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setInformation({ country: data.country_name, city: data.city });
+      });
+  };
+
+  const delateRow = (id) => {
+    fetch(`/api/items/${id}`, {
+        method: 'DELETE',
+      })
   };
 
   return (
     <div className="container">
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-5">
-          <table className="table border">
+          <table className="table border table-striped">
             <thead>
               <tr>
                 <th>Num</th>
                 <th>Ip address</th>
                 <th>Date and time</th>
                 <th>Delete</th>
+                <th>View</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
               {ipAddress?.map((el, index) => {
                 const dataFormat = new Date(el.createdAt).toLocaleString();
                 return (
-                  <tr key={el._id} onClick={findData}>
+                  <tr key={el._id}>
                     <td>{index + 1}</td>
                     <td>{el.ip}</td>
                     <td>{dataFormat}</td>
                     <td>
-                      <button className="btn btn-sm btn-danger">Delete</button>
+                      <button
+                        className="btn btn-sm btn-danger"
+                        onClick={() => delateRow(el._id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-sm btn-success "
+                        onClick={() => findData(el.ip)}
+                      >
+                        View
+                      </button>
                     </td>
                   </tr>
                 );
               })}
+            </tbody>
+          </table>
+        </div>
+        <div className="col-5 offset-2">
+          <table className="table border">
+            <tbody>
+              <tr>
+                <th scope="row">Country</th>
+                <td>{information.country}</td>
+              </tr>
+              <tr>
+                <th scope="row">City</th>
+                <td>{information.city}</td>
+              </tr>
             </tbody>
           </table>
         </div>
