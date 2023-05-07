d4ab427dddc8e6145e945d9648579996f2aaa28d
Milos1988 <vojvoda1988>
2023-05-07 15:42:59 +0200

    Change data format

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,43 +1,52 @@
-import { useEffect, useState } from "react"
-import { getAllData } from "../service/service"
+import { useEffect, useState } from "react";
+import { getAllData } from "../service/service";
 
 const HomePage = () => {
-    const [ipAddress, setIpAddress] = useState()
-        
+  const [ipAddress, setIpAddress] = useState();
 
-        useEffect(() => {
-            getAllData().then(data => setIpAddress(data.data))
-            .catch( error => console.log(error))
-        },[]
-        )
-    
-return(
+  useEffect(() => {
+    getAllData()
+      .then((data) => setIpAddress(data.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const findData = () => {
+    console.log("work");
+  };
+
+  return (
     <div className="container">
-        <div className="row">
-            <div className="col-3">
-      <table className="table ">
-        <thead>
-            <tr><th>
-            Ip address
-                </th></tr>
-           </thead>
-           <tbody>
-            {
-                ipAddress?.map( el => {
-                    return(
-                        <tr key={el._id}>
-                            <td>{el.ip}</td>
-                        </tr>
-                    )
-                })
-            }
-           </tbody>
-
-      </table>
-      </div>
+      <div className="row">
+        <div className="col-5">
+          <table className="table border">
+            <thead>
+              <tr>
+                <th>Num</th>
+                <th>Ip address</th>
+                <th>Date and time</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {ipAddress?.map((el, index) => {
+                const dataFormat = new Date(el.createdAt).toLocaleString();
+                return (
+                  <tr key={el._id} onClick={findData}>
+                    <td>{index + 1}</td>
+                    <td>{el.ip}</td>
+                    <td>{dataFormat}</td>
+                    <td>
+                      <button className="btn btn-sm btn-danger">Delete</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-)
-}
+  );
+};
 
-export default HomePage 
+export default HomePage;
